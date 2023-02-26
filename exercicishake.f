9d662763ca26930cc27aa40f817558ad1de9e5b3
Elena G <elena.gardela@gmail.com>
2023-02-26 12:38:54 +0100

    radial ditribution working

--- exercicishake.f
+++ exercicishake.f
@@ -65,7 +65,9 @@ c     5. Comença el bucle de la generacio de configuracions
       open(99, file='thermdata.out', status='replace')
       write(99,*) ("Iter, ekin, epot, etot, temp, lambda")
 
-      allocate(gr_mat(num_bins))
+      allocate(gr_mat(2,num_bins))
+      call g_r(nmolecules, natoms,gr_mat, r, num_bins, costat, 1)
+      
       do i = 1,nconf
          call forces(nmolecules,natoms,r,costat,accel,rc,epot)
          call factlambda(nmolecules,natoms,vinf,nf,tempref,
@@ -80,9 +82,21 @@ c
      &temperatura,nf,ecin)
          write(99,*) (i,ecin,epot,ecin+epot,temperatura,lambda,l=1,1)
          
-         call g_r(gr_mat, r, num_bins, costat, switch_case)
+         call g_r(nmolecules, natoms,gr_mat, r, num_bins, costat, 2)
+     
       end do
       close(99)
+
+c     Escriptura g(r)
+      open(22, file='radial_func.out', status='replace')
+      write(22,*) ("dr, g(r)")
+      call g_r(nmolecules, natoms,gr_mat, r, num_bins, costat, 3)
+      do  j=1,num_bins 
+      write(22,*)  gr_mat(1,j),gr_mat(2,j)
+      enddo
+      close(22)
+      
+      
 c     5. Escriptura de la darrera configuracio en A i A/ps 
 
       open(11,file='confnova.data',status='unknown')
@@ -312,32 +326,34 @@ c              subrutina radial distribution
 *********************************************************
 *********************************************************
       
-      subroutine g_r(gr_mat, r, num_bins, box_size, switch_case)
-      implicit double precision(a-h,o-z)
-      integer(kind=i64), intent(in)      :: switch_case
+      subroutine g_r(nmolecules, natoms,gr_mat, r, num_bins, 
+     &box_size, switch_case)
       
-      integer(kind=i64), save                      :: i, index_mat, j, total_part, n_gdr
-      real(kind=dp), save                          :: dr, dist, dv, ndg, dens
+      implicit double precision(a-h,o-z)
+      integer, intent(in)      :: switch_case
+      real*8, parameter        :: PI = 4.d0 * datan(1.d0)
+      integer, save            :: i, index_mat,total_part, n_gdr
+      real*8, save             :: dr, dist, dv, ndg, dens
       include 'exercicishake.dim'
       dimension r(3,nmax,nmaxmol), gr_mat(2,num_bins), rij(3)
       
       
        select case (switch_case)
-            case (1_i64)
-            	n_gdr = 0_i64
+            case (1)
+            	n_gdr = 0
       		total_part=nmax*nmaxmol
       
-      		dr = box_size / real(2*num_bins, kind=dp)
-     		dens = real(total_part, kind=dp) / (box_size ** 3)
+      		dr = box_size / dble(2*num_bins)
+     		dens = dble(total_part) / (box_size ** 3)
 
-       		gr_mat(1,:) = [(real(i, kind=dp)*dr, i=1, num_bins)]
-       		gr_mat(2,:) = 0.0_dp
+       		gr_mat(1,:) = [(dble(i)*dr, i=1, num_bins)]
+       		gr_mat(2,:) = 0.d0
        		
-       	    case (2_i64)
-       	    	n_gdr = n_gdr + 1_i64
+       	    case (2)
+       	    	n_gdr = n_gdr + 1
        	    	
 	        do ic = 1,nmolecules-1
-		  do is = 1,nmax
+		  do is = 1,natoms
 		    do jc = ic+1,nmolecules
 		       do js = 1,natoms
 			  rij(:)=r(:,js,jc)-r(:,is,ic)
@@ -347,8 +363,8 @@ c              subrutina radial distribution
 			  
 			  ! Apliquem el cutoff de maxima distancia
                           if (dist .lt. box_size/2) then
-                              index_mat = int(dist/dr, kind=i64) + 1_i64
-                              gr_mat(2, index_mat) = gr_mat(2, index_mat) + 2.0_dp
+                            index_mat = int(dist/dr) + 1
+                            gr_mat(2,index_mat)=gr_mat(2,index_mat)+2.d0
                           end if
 		       end do
 		    end do
@@ -356,21 +372,17 @@ c              subrutina radial distribution
 	        end do
         
               
-             case (3_i64)
+             case (3)
         
                 ! SWITCH = 3 => Calculem g(r)
-                open(11, file='radial_distribution.txt',status='unknown',access='append', action="write")
- 
+                
                 do i = 1, num_bins
-                    associate(gdr => gr_mat(2,i))
-                        dv = (((real(i, kind=dp) + 1.0_dp) ** 3) - (real(i, kind=dp) ** 3)) * (dr ** 3)
-                        ndg = (4.0_dp / 3.0_dp) * pi * dv * dens
-                        gdr = gdr / (real(total_part, kind=dp) * ndg * real(n_gdr, kind=dp))
-                    end associate
-                    write(11,*)   gr_mat(1,i), gr_mat(2,i)
+                 associate(gdr => gr_mat(2,i))
+                  dv = (((dble(i) + 1.d0)**3)-(dble(i)**3))*(dr**3)
+                  ndg = (4.d0/ 3.d0) * pi * dv * dens
+                  gdr = gdr / (dble(total_part * ndg * n_gdr))
+                 end associate
                 end do
-                write(1,*) " "
-                close(1)
             end select
             
       end subroutine g_r
